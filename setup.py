529dfdd9b01f314688768cf7a4410786c5cc6e57
LinuxPhreak <ben@bigbenshosting.com>
2019-02-10 18:55:46 -0500

    Made changes based on https://github.com/pypa/sampleproject

--- setup.py
+++ setup.py
@@ -1,16 +1,57 @@
 #!/usr/bin/env python
-
 from setuptools import setup, find_packages
+from os import path
+from io import open
 
-setup(name='Username',
-      version='1.0',
-      description='Search Usernames online',
-      author='Ben P. Dorsi-Todaro',
-      author_email='ben@bigbenshosting.com',
-      license='GPL 3',
-      classifiers=['Python3.7','Python 3.6'],
-      platform='Linux',
-      url='https://linuxphreak.github.io/username/',
-      packages=find_packages(),
-      install_requires=['requests','bs4']
-     )
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Username',
+    # https://www.python.org/dev/peps/pep-0440/
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0.0.0',  # Required
+    # https://packaging.python.org/specifications/core-metadata/#summary
+    description='Search for usernames online to see if they are availible or not',  # Optional
+    # https://packaging.python.org/specifications/core-metadata/#description-optional
+    long_description=long_description,  # Optional
+    # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
+    url='https://linuxphreak.github.io/username',  # Optional
+    author='Ben P. Dorsi-Todaro',  # Optional
+    author_email='ben@bigbenshosting.com',  # Optional
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Researchers',
+        'Topic :: Aliases and Usernames Online :: Background Checks',
+        'License :: GNU :: GPL',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    # This field adds keywords for your project which will appear on the
+    # project page. What does your project relate to?
+    #
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords='username search',  # Optional
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+
+    # Specify which Python versions you support. In contrast to the
+    # 'Programming Language' classifiers above, 'pip install' will check this
+    # and refuse to install the project if the version does not match. If you
+    # do not support Python 2, you can simplify this to '>=3.5' or similar, see
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
+    python_requires='<=3.7.*',
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['requests','os','beautifulsoup4'],  # Optional
+)
